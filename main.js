405ef48c38b25222e4d752bb085b05692933015b
Amit Kushwaha <akkushwaha018@gmail.com>
2023-03-26 16:27:04 +0530

    First Commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function myFunction() {
+        document.getElementById("show_after").style.display = "block";
+}
+function myFunction1() {
+        document.getElementById("show_after1").style.display = "block";
+}
+function myFunction2() {
+        document.getElementById("show_after2").style.display = "block";
+}
